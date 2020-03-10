362bd55404cfcf16513a9a991372a1d6fd4b58c6
DJ0ker <djoubaud@gmail.com>
2020-03-10 19:07:33 +0100

    interface.h Lot_A.4 by Joker

---
+++ headers/interface.h
@@ -0,0 +1,58 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+
+/*
+requires the current turn currturn
+assigns 
+ensures create a new turn :
+		add 1 to currturn, if turn 6 display fix to 2 the nb of 'personnel' card, if turn 11 fix to 3 the nb of 'personnel' card */
+void newturn(int currturn);
+
+/*requires player equals to 0 or 1 (ENSIIE A or ENSIIE B)
+assigns
+ensures display "Nouvelle Phase" and the player associate to the new phase, player = (player + 1) mod 2
+*/
+void newphase(int player);
+
+/*Let s define the cardboard as a structure with 3 categories : Leleves, Lpersonnel, Lactions which represent the card present 
+on 1 player s board. each composante is a list with the diffent cards on the board
+*/
+
+typedef struct cardsonboard cob;
+struct cardsonboard
+{
+	char* Leleves;
+	char* Lpersonnel;
+	char* Lactions;
+	
+};
+
+/*requires matrix of player1's cards idem for player2,
+assigns
+ensures display the board of each player
+*/
+
+void board(cob p1card, cob p2cards);
+
+/*requires player 0 or 1
+ * assigns
+ * ensures give a FISE or FISA card to the player concerned
+ */
+void FiseOrFisa(int player);
+
+/*requires player 0 or 1, player s PE
+ * assigns 
+ * ensures The function ask the player what to do : play a card or finish the turn. 
+ *         The function only propose the card which cost less than player s PE. If no card
+ *         available then the only choice its end the turn
+*/
+void Choice(int player, int PE);
+
+
+/*requires player 0 or 1 or other
+ * assigns
+ * ensures display the winner (player 0 o 1), if player !=0 or 1 then display "égalité"
+*/
+
+void EndGame(int player);

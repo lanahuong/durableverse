e3703a89721c24b29acc0193106d0d4d34e570b8
unknown <caroline.cao@ensiie.fr>
2020-03-18 17:42:07 +0100

    Modif de carte.h : retirer les "plateaux"

--- headers/carte.h
+++ headers/carte.h
@@ -9,39 +9,39 @@
 int receive_student_card(ensiie player, int turn_nb_prev);
 
 /*
-@requires cardboard p and card c
-@assigns add a malloc on the cardboard and put c on it 
-@ensures add a cad on the cardboard 
+@requires player and card c
+@assigns add a malloc on player's cardboard and put c on it 
+@ensures add a card on the cardboard of player 
 */
-void add_student_card(plateau *p, carte c);
+void add_student_card(ensiie *player, carte c);
 
 /*
 @requires current turn and the player
 @assigns nothing
-@ensures return hte number of PE available forthe player in the current turn 
+@ensures return the number of PE available for the player in the current turn 
 */
 int available_PE(ensiie player, int current_turn);
 
 /*
-@requires player and the card c he want's to play on the cardboard p
-@assigns add a malloc on the cardboard and put c on it
+@requires player and the card c one wants to play on its cardboard 
+@assigns add a malloc on player's cardboard and put c on it
 @ensures put the card c on the cardboard 
 */
-void play_card(ensiie player, carte c, plateau *p);
+void play_card(ensiie player, carte c);
 
 /*
-@requires cardboard p 
+@requires player's cardboard 
 @assigns nothing
 @ensures return the number of DD earned by each player "player x : y DD " y could be positive or negative
 */
-char DD_earned(plateau p);
+char DD_earned(ensiie player);
 
 /*
-@requires cardboard p, player 1, player 2
+@requires player 1, player 2 
 @assigns nothing
 @ensures tells if the game is over and if someone wins 
 */
-char is_over(plateau p, ensiie player1, ensiie player2);
+char is_over(ensiie player1, ensiie player2);
 
 #ENDIF
 

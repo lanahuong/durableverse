c4fa3788a5722bcebe771dc755425fe5c1e746f9
DJ0ker <djoubaud@gmail.com>
2020-03-18 17:43:16 +0100

    interface.h v6 by joker

--- headers/interface.h
+++ headers/interface.h
@@ -7,13 +7,13 @@ requires the current turn currturn
 assigns 
 ensures create a new turn :
 		add 1 to currturn, if turn 6 display fix to 2 the nb of 'personnel' card, if turn 11 fix to 3 the nb of 'personnel' card */
-void newturn(int* currturn);
+void  interfaceNewTurn(int* currturn);
 
 /*requires player equals to 0 or 1 (ENSIIE A or ENSIIE B)
 assigns
 ensures display "Nouvelle Phase" and the player associate to the new phase, player = (player + 1) mod 2
 */
-void NewPhase(ensiie player);
+void interfaceNewPhase(ensiie player);
 
 /*Let s define the cardboard as a structure with 3 categories : Leleves, Lpersonnel, Lactions which represent the card present 
 on 1 player s board. each composante is a list with the diffent cards on the board
@@ -33,13 +33,13 @@ assigns
 ensures display the board of each player
 */
 
-void board(cob* p1card, cob* p2cards);
+void interfaceBoard(cob* p1card, cob* p2cards);
 
 /*requires player 0 or 1
  * assigns
  * ensures give a FISE or FISA card to the player concerned
  */
-void fiseorfisa(ensiie player);
+void interfaceFiseOrFisa(ensiie player);
 
 /*requires player 0 or 1
  * assigns 
@@ -47,7 +47,7 @@ void fiseorfisa(ensiie player);
  *         The function only propose the card which cost less than player s PE. If no card
  *         available then the only choice its end the turn
 */
-void choice(ensiie player);
+void interfaceChoice(ensiie player);
 
 
 /*requires player 0 or 1 
@@ -55,6 +55,6 @@ void choice(ensiie player);
  * ensures display the winner (player 0 o 1)
  * */
 
-void endgame(ensiie player);
+void interfaceEndGame(ensiie player);
 
 #endif

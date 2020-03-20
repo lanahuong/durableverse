4305cf9b81d4b8ce1b25379f4fd0bdbdc4cced53
Lana Scravaglieri <lana.scravaglieri@gmail.com>
2020-03-20 18:29:42 +0100

    main function completed and auxilary functions full prototypes and description

--- src/main.c
+++ src/main.c
@@ -23,29 +23,46 @@ typedef struct ensiie ensiie;
 
 /* 
  * Handle the playing phase of one player
- * @require
- * @assigns
- * @ensures
+ * @require nothing
+ * @assigns players' board, deck, hand and discard
+ * @ensures nothing
  */
-void phase(){
+void phase(ensiie player){
 }
 
-/* Handle on turn off the game
- * @require
- * @assigns
- * @ensures
+/* Handle one turn off the game
+ * @require nothing
+ * @assigns players' DD
+ * @ensures nothing
  */
-void gameLoop(){
+void gameLoop(int turn, ensiie players[2]){
 }
 
 /* Initialize the whole game
- * @require
- * @assigns
- * @ensures
+ * @require nothing
+ * @assigns turn=0, winner=-1, players have a full deck, empty hand, discard and
+ *          board, 0 PE and 0 DD
+ * @ensures nothing
  */
-void gameSetUp(){
+void gameSetUp(ensiie players[2], int* turn, int* winner){
 }
 
 int main(int argc, char** argv) {
+  // Initialization of game variables
+  ensiie players[2];
+  int turn;
+  int winner;
+  gameSetUp(players, &turn, &winner);
+
+  // While the game isn't over an other turn plays out
+  while (turn<=30 || winner) {
+    turn++;
+    gameLoop(turn, players);
+    winner = cardIsOver(players);
+  }
+  
+  // When the game is over display the outcome
+  interfaceEndGame(winner);
+
   return 0;
 }

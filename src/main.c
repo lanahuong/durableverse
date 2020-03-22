1ae63517e4d9840c4d9f85f6ba97fc4856a3820a
Lana Scravaglieri <lana.scravaglieri@gmail.com>
2020-03-22 16:22:31 +0100

    gameSetUp function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 // Structure to store cards for the deck, the hand and the discard
 struct cardList {
   int length;
-  int cards[42];
+  int *cards;
 };
 typedef struct cardList cardList;
 
@@ -20,6 +20,9 @@ struct ensiie {
   cardList discard;
 };
 typedef struct ensiie ensiie;
+
+int const NB_CARDS = 42;
+int const DEFAULT_DECK[NB_CARDS] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,21,21,22,22,22,23,23,24,24,25,25,26,26,27,27,27,27,28,29,30,31};
 
 /* 
  * Handle the playing phase of one player
@@ -45,6 +48,20 @@ void gameLoop(int turn, ensiie players[2]){
  * @ensures nothing
  */
 void gameSetUp(ensiie players[2], int* turn, int* winner){
+  *winner = -1;
+  *turn = 0;
+  
+  for (int i=0; i<2 ; i++){
+    players[i].DD = 0;
+    players[i].PE = 0;
+    players[i].hand.length = 0;
+    players[i].discard.length = 0;
+    players[i].deck.length = NB_CARDS;
+    players[i].deck.cards = malloc(NB_CARDS*sizeof(int));
+    for (int k=0; k<NB_CARDS; k++) {
+      players[i].deck.cards[k] = DEFAULT_DECK[k];
+    }
+  }
 }
 
 int main(int argc, char** argv) {

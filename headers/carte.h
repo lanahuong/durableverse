3488487413ebe202198c38541191054893e3d711
unknown <caroline.cao@ensiie.fr>
2020-03-23 18:51:55 +0100

    Modification de carte.h puis transfert à plateau.h __Chipie

--- headers/carte.h
+++ headers/carte.h
@@ -12,49 +12,6 @@ struct Carte
   int num;
 };
 
-typedef int ensiie;
-
-/*
-@requires player who will receive its student cards
-@assigns nothing
-@ensures return the number of card the player will receive 
-*/
-int receive_student_card(ensiie player);
-
-/*
-@requires player and student card c onr wants to dd on its cardboard
-@assigns add a malloc on player's cardboard on FILO side and put c on it 
-@ensures add the student card c on one of the FILO from the player's cardboard  
-*/
-void add_student_card(ensiie *player, Carte c);
-
-/*
-@requires player
-@assigns nothing
-@ensures return the number of PE available for the player in the current turn 
-*/
-int available_PE(ensiie player);
-
-/*
-@requires player and the card c one wants to play on its cardboard 
-@assigns add a malloc on player's cardboard and put c on it
-@ensures put the card c on the cardboard 
-*/
-void play_card(ensiie player, Carte c);
-
-/*
-@requires player's cardboard 
-@assigns nothing
-@ensures return the number of DD earned by each player "player x : y DD " y could be positive or negative
-*/
-char DD_earned(ensiie player);
-
-/*
-@requires player 1 and player 2 
-@assigns nothing
-@ensures tells if the game is over and if someone wins 
-*/
-char is_over(ensiie player1, ensiie player2);
 
 #endif
 
--- headers/plateau.h
+++ headers/plateau.h
@@ -45,4 +45,47 @@ int nbr_cd_phaseNew(Plateau p);
 
 void pioche(Plateau *p);
 
+/*
+@requires player's cardboard who will receive its student cards
+@assigns nothing
+@ensures return the number of card the player will receive according to its cardboard
+*/
+int receive_student_card(Plateau p);
+
+/*
+@requires player's cardboard and student card c one wants to play on its cardboard
+@assigns add a malloc on player's cardboard on FILO side and put c on it 
+@ensures add the student card c on one of the FILO from the player's cardboard  
+*/
+void play_student_card(Plateau p, Carte c);
+
+/*
+@requires player's cardboard and the current turn
+@assigns nothing
+@ensures return the number of PE available for the player in the current turn 
+*/
+int available_PE(Plateau p, int curturn);
+
+/*
+@requires player's cardboard and the card c one wants to play on its cardboard 
+@assigns add a malloc on player's cardboard and put c on it
+@ensures put the card c on the cardboard 
+*/
+void play_card(Plateau p, Carte c);
+
+/*
+@requires players' cardboards 
+@assigns nothing
+@ensures return the number of DD earned by each player "player x : y DD " y could be positive or negative
+*/
+char DD_earned(Plateau p1, Plateau p2);
+
+/*
+@requires DD from player 1 and player 2 and the current turn
+@assigns nothing
+@ensures tells if the game is over and if someone wins 
+*/
+char is_over(int DDp1,int DDp2, int curturn);
+
+
 #endif

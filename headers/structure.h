105a71c80ad8b47eae3ed0c404124de5774249c2
Lana Scravaglieri <lana.scravaglieri@gmail.com>
2020-05-24 23:58:39 +0200

    Fix comments for Doxygene doc

--- headers/structure.h
+++ headers/structure.h
@@ -1,7 +1,12 @@
 #ifndef STRUCTURE_H
 #define STRUCTURE_H
 
-
+/**
+ * \file structure.h
+ *
+ * This file describes all the function used to create and manipulate cardList
+ * and queue data structures.
+ */
 
 // To set a structure for the deck
 typedef struct cardList {
@@ -9,57 +14,64 @@ typedef struct cardList {
   int* content;
 } cardList; 
 
-/* 
-@requires a correctly formated cardList
-@assigns nothing
-@ensures return the length */
+/**
+ * \brief length getter
+ * \param l a cardList
+ * \return the length of l
+ */
 int structure_getCardListLength(cardList* l);
 
-/* 
-@requires a correctly formated cardList
-@assigns nothing
-@ensures return the content */
+/**
+ * \brief content getter
+ * \param l a cardList
+ * \return the content array of l
+ */
 int* structure_getCardListContent(cardList* l);
 
-/*
-@requires nothing
-@assigns nothing
-@ensures return an 42-sized empty cardList 
-*/
+/**
+ * \brief Create an empty cardList of maximum size 42
+ */
 cardList* structure_emptyCardList();
-/*
-@requires a correctly formated cardList
-@assigns nothing
-@ensures return 0 if the cardList is empty else 1
-*/
+
+/**
+ * \brief Test if a cardList is empty
+ * \param cl a cardList
+ * \return 1 if the list is empty else 0
+ */
 int structure_isEmptyCardList(cardList* cl);
-/* 
-@requires correctly formatted cardList cl and card number c
-@assigns cardList 
-@ensures add the card which number is c to the cardList cl
-*/
+
+/**
+ * \brief Add a card to a cardList
+ * \param cl a calrdList
+ * \param c the number of the card to add
+ * \internal increament the length of the list and set the length-1 th
+ *           element content array to \a c
+ */
 void structure_addCardCardList(cardList* cl, int c);
 
-/* 
-@requires correctly formatted cardList cl and an int c smaller than cardList.length
-@assigns cardList 
-@ensures remove the card which index is c from the cardList cl and return the card's number
-*/
+/**
+ * \brief Remove the card at the given index from a cardList
+ * \param cL a cardList
+ * \param c the index of the card to remove
+ * \internal swap the last card (of index length-1) and the one to remove and
+ *           decrease the length
+ * \return the number of the card removed
+ */
 int structure_removeCardCardList(cardList* cl, int c);
 
-/*
-@requires a correctly formatted cardList cl
-@assigns queue
-@ensures free the cardList cl
-*/
+/**
+ * \brief Free the memory space used by a cardList
+ * \param cl a cardList
+ * \internal free the content and free itself
+ */
 void structure_freeCardList(cardList* cl);
 
-/*
-@requires correctly formatted cardList cl and card number c
-@assigns nothing
-@ensures return the index of the card which number is c in cl or 
-                    -1 if that card does not exist in cl
-*/
+/**
+ * \brief Search a card in a cardList given its number
+ * \param cl the cardList to search
+ * \param c the number of the card to search for
+ * \return the index of the card if it's found else -1
+ */
 int structure_searchCardList(cardList* cl, int c);
 
 
@@ -73,48 +85,49 @@ typedef struct queue {
     int* content;
 } queue;
 
-/*
-@requires an unsigned int
-@assigns memory space for a capacity-sized queue of int 
-@ensures create a queue */
+/**
+ * \brief Create an empty queue
+ * \return the newly created empty queue
+ */
 queue* structure_emptyQueue();
 
-/*
-@requires a correctly formated queue
-@assigns nothing
-@ensures return 1 if the queue is empty else 0 */
+/**
+ * \brief Test if a queue is empty
+ * \return 1 if the queue is empty else 0
+ */
 int structure_isEmptyQueue(queue* q);
 
-/*
-@requires a correctly formated queue
-@assigns nothing
-@ensures return 1 if the queue is full else 0 */
+/**
+ * \brief Test if a queue is full
+ * \return 1 if the queue is full else 0
+ */
 int structure_isFullQueue(queue* q);
 
-/*
-@requires a correctly formated non full queue and an integer n
-@assigns content, size and last
-@ensures add n to the queue */
+/**
+ * \brief Add an element to a non full queue
+ */
 void structure_enqueue(queue* q, int n);
 
-/*
-@requires a correctly formated non empty queue
-@assigns content, size and first
-@ensures remove the first item of the queue and return it */
+/**
+ * \brief Remove an element from a non empty queue
+ */
 int structure_dequeue(queue* q);
 
-/* 
-@requires a correctly formated queue
-@assigns nothing
-@ensures return the size */
+/**
+ * \brief size getter
+ * \return the size of the queue (numbr of elements it contains)
+ */
 int structure_getQueueSize(queue* l);
 
-/* 
-@requires a correctly formated queue
-@assigns nothing
-@ensures return the content */
+/**
+ * \brief content getter
+ * \return the content of the queue
+ */
 int* structure_getQueueContent(queue* l);
 
+/**
+ * \brief Free the memory space used by a queue
+ */
 /*
 @requires a correctly formatted queue q
 @assigns queue
